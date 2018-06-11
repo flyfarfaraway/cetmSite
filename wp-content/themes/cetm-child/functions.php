b69f5b771111991ea1b1b08dd20ccce87ec414af
hieudt <hieudt@miraway.vn>
2018-06-11 12:05:59 +0700

    checkin wweb

--- wp-content/themes/cetm-child/functions.php
+++ wp-content/themes/cetm-child/functions.php
@@ -1,2 +1,9 @@
 <?php
 // Add custom Theme Functions here
+
+function cetm_scripts()
+{
+    // wp_enqueue_style('odometer.min', get_stylesheet_directory_uri() . '/style.css', array(), '');
+    // wp_enqueue_style('bootstrap.min', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '');
+}
+add_action('wp_enqueue_scripts', 'cetm_scripts');
