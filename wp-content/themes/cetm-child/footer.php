e21b338765e63db76a30d3ed86859e9eef6e0f26
datpt <datpt@miraway.vn>
2018-06-18 08:54:14 +0700

    update 18/6

---
+++ wp-content/themes/cetm-child/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package flatsome
+ */
+
+global $flatsome_opt;
+?>
+
+</main><!-- #main -->
+
+<footer>
+    <?php if (is_active_sidebar('sidebar-footer-2') && get_theme_mod('footer_2', 1)) : ?>
+        <div class="container">
+            <div class="row clearfix">
+                <?php dynamic_sidebar('sidebar-footer-2'); ?>
+            </div>
+        </div>
+    <?php endif; ?>
+    <!--	--><?php //do_action('flatsome_footer'); ?>
+
+</footer><!-- .footer-wrapper -->
+
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wp-content/themes/cetm-child/functions.php
+++ wp-content/themes/cetm-child/functions.php
@@ -3,7 +3,23 @@
 
 function cetm_scripts()
 {
-    // wp_enqueue_style('odometer.min', get_stylesheet_directory_uri() . '/style.css', array(), '');
+    wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/styles/styles.css', array(), '');
+    wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css', array(), '');
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/styles/bootstrap.css', array(), '');
+    wp_enqueue_style('swiper', get_stylesheet_directory_uri() . '/styles/lib/swiper.css', array(), '');
+    wp_enqueue_style('fontawesome', get_stylesheet_directory_uri() . '/styles/fontawesome-all.css', array(), '');
+
+    if (!is_user_logged_in() && is_front_page()) {
+        wp_enqueue_script('jqueryjs', get_stylesheet_directory_uri() . '/scripts/jquery.js', array(), '');
+        wp_enqueue_script('bootstrapjs', get_stylesheet_directory_uri() . '/scripts/bootstrap.js', array(), '');
+        wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/scripts/custom.js', array(), '');
+        wp_enqueue_script('swiperjs', get_stylesheet_directory_uri() . '/scripts/swiper.js', array(), '');
+        wp_enqueue_script('slick', get_stylesheet_directory_uri() . '/scripts/slick.min.js', array(), '');
+        wp_enqueue_script('home', get_stylesheet_directory_uri() . '/scripts/page/home.js', array(), '');
+    }
+
+
     // wp_enqueue_style('bootstrap.min', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '');
 }
+
 add_action('wp_enqueue_scripts', 'cetm_scripts');
